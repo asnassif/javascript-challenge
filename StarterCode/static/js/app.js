4ef7e1713556d76cb5e14603a8eb18c875299233
Amgad <you@example.com>
2019-12-27 05:29:58 -0500

    update

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,45 @@
-// from data.js
-var tableData = data;
 
-// YOUR CODE HERE!
+let tbody = d3.select("tbody");
+
+let getMatchingRecords = dt => {
+    let mdy1 = new Date(dt);
+    // console.log(mdy1.getMonth());
+    // console.log(mdy1.getDate());
+    // console.log(mdy1.getFullYear());
+    let sightings = []
+    data.forEach(datum => {
+        let mdy2 = new Date(datum.datetime);
+        if ((mdy2.getTime() === mdy1.getTime()) || (dt === "")) {
+            sightings.push(datum);
+        }
+    });
+    return sightings;
+}
+
+let updateTable = sightings => {
+    tbody.html("");
+    if (sightings.length < 1) return;
+    sightings.forEach(record => {
+        let row = tbody.append("tr");
+        Object.entries(record).forEach(([key, value]) => {
+            let cell = row.append("td");
+            cell.text(value);
+        });
+    });
+}
+
+let button = d3.select("#filter-btn");
+
+let handleInput = () => {
+    // stops the page refresh on "Enter" button
+    d3.event.preventDefault();
+
+    let dt = d3.select("#datetime").property("value");
+    let sightings = getMatchingRecords(dt);
+    updateTable(sightings);
+}
+
+// Update the table with button click
+button.on("click", handleInput);
+// Enter also update the output
+d3.select("form").on("submit", handleInput);
